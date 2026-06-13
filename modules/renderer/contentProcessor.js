69a3f71b713bea8ed90c319e35be7663c34b51c1
lioensky <lionsky75@gmail.com>
2026-06-13 12:23:31 +0800

    小优化

--- modules/renderer/contentProcessor.js
+++ modules/renderer/contentProcessor.js
@@ -990,6 +990,7 @@ function processRenderedContent(contentDiv, settings = {}) {
         window.renderMathInElement(contentDiv, {
             delimiters: [
                 {left: "$$", right: "$$", display: true},
+                {left: "$", right: "$", display: false},
                 {left: "\\(", right: "\\)", display: false},
                 {left: "\\[", right: "\\]", display: true}
             ],
